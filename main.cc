4519990bc0785ad4d470ebf0c3f6f3cb534caff2
Jerf <jeremy.forget@epita.fr>
2015-10-19 16:40:10 +0200

    sphere function ok

--- main.cc
+++ main.cc
@@ -26,17 +26,13 @@ int main(int argc, char** argv)
     int n = SDL_Init(SDL_INIT_EVERYTHING);
     if (n < 0)
         printf("fail SDL: %s\n", SDL_GetError());
-        return 1;
     SDL_Window* w = SDL_CreateWindow("test", SDL_WINDOWPOS_UNDEFINED,
     SDL_WINDOWPOS_UNDEFINED, 200, 200, SDL_WINDOW_SHOWN);
     SDL_Surface* screen = SDL_GetWindowSurface(w);
     Camera cam = Camera(100,100,100);
     SDL_Surface* s = SDL_CreateRGBSurface(0,200,200,32,0,0,0,0);
     Sphere d = Sphere(100,100, 0, 50);
-    //launch(s, cam, d);
-    for (int i = 0; i < s->w; i++)
-        for (int j = 0; j < s->h; j++)
-            setPixel(s, i, j, SDL_MapRGB(s->format, 255,255,255));
+    launch(s, cam, d);
     SDL_BlitSurface(s, NULL, screen, NULL);
     SDL_UpdateWindowSurface(w);
     SDL_Delay(1000);
